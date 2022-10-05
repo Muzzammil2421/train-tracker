4f675f4557d2469392c14dd9528de816fe7e89b9
Ahmed Badr <ahmed.k.badr.97@gmail.com>
2022-10-05 22:42:29 +0200

    add setup.py and requirnments,txt

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # train-tracker
-library you can use to track your training data while you are training your pytorch model ,
-track the architecture change , loss function change , optimizer change , learning rate and save all this in a csv file also another csv file saving epochs data and how this changes affecting the train loss , test loss   
+- library you can use to track your training data while you are training your pytorch model ,
+track the architecture change , loss function change , optimizer change , learning rate and save all this in a csv file also another csv file saving epochs data and how this changes affecting the train loss , test loss
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='train-tracker',
+    version='0.0.1',
+    author='Ahmed badr',
+    author_email='ahmed.k.badr.97@gmail.com',
+    description='library you can use to track your training data while you are training your pytorch model',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    url='https://github.com/ahmedbadr97/train-tracker',
+    license='MIT',
+    packages=['traintracker'],
+    package_dir={
+        'traintracker': 'src/traintracker','convfacenet.face_descriptor': 'src/convfacenet/face_descriptor','convfacenet.face_detector':'src/convfacenet/face_detector'},
+    install_requires=['torch','numpy','pandas'],
+)
